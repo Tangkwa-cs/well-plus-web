a23d4af7ee97934ce7ccca5b201d1099d68ba5b5
m <suravut.k@ampd.co.th>
2022-02-15 15:42:12 +0700

    add rewind swiper

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import SwiperCore, { Swiper, Virtual, EffectFade ,loop} from 'swiper';
+import SwiperCore, { Swiper, Virtual, EffectFade, SwiperOptions } from 'swiper';
 import { SwiperComponent } from "swiper/angular";
 import 'swiper/css';
 import 'swiper/less/pagination';
@@ -16,6 +16,9 @@ SwiperCore.use([EffectFade]);
 })
 
 export class HomeComponent implements OnInit {
+  config: SwiperOptions = {
+    rewind:true
+  };
   imageHouse = ["มัณ1.png","มัณ2.png","มัณ3.png","มัณ4.png","มัณ5.png","มัณ6.png","มัณ7.png","มัณ8.png","มัณ9.png","มัณ10.png","มัณ11.png","มัณ13.png","มัณ14.png","มัณ15.png","มัณ16.png"];
   imagewall = ["นิรัน1.png","นิรัน2.png","นิรัน3.png"];
   imageRes = ["เอเชียทีค1.png","เอเชียทีค2.png","เอเชียทีค3.png","เอเชียทีค4.png","เอเชียทีค5.png","เอเชียทีค6.png","เอเชียทีค7.png","เอเชียทีค8.png","เอเชียทีค9.png"];
